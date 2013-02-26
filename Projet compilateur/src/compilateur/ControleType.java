58f28d72fea49d044d6c27bf7180368023fdd9ad
Damien Crémilleux <damien.cremilleux@insa-rennes.fr>
2013-02-26 08:37:13 +0100

    Ajout de la classe ControleType

---
+++ Projet compilateur/src/compilateur/ControleType.java
@@ -0,0 +1,27 @@
+/**
+ * Project compilateur - 2012/2013
+ * date : 19/02/2013
+ * 
+ */
+package compilateur;
+
+/**
+ * Classe ControleType
+ * 
+ * @author Samuel COZ - Damien CRÉMILLEUX - Lauriane HOLY - Arnaud TROUCHE
+ * 
+ */
+
+import java.util.List;
+import java.util.Stack;
+
+public class ControleType {
+    public List<String> pileT = new Stack<String> ();
+
+
+    public int controlerType() {
+        // TODO Auto-generated return
+        return 0;
+    }
+
+}
