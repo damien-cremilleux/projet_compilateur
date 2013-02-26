4f7c37f0677fe72df095e83b8dd670fe04a31f0c
scoz <scoz@PC2-INFO8.educ.insa>
2013-02-26 09:48:49 +0100

    Codage d'Expression (ajouter aux différentes piles) Modification type pile des valeurs integer->OBJECT pour la généralité des valeurs
    Yaka.jj : ajout de la variable controleT qui fait référence à la classe
    ControleType

--- Projet compilateur/src/compilateur/Expression.java
+++ Projet compilateur/src/compilateur/Expression.java
@@ -5,7 +5,6 @@
  */
 package compilateur;
 
-import java.util.List;
 import java.util.Stack;
 
 /**
@@ -18,125 +17,49 @@ public class Expression {
     /**
      * Pile contenant les opérandes 
      */
-    public List<Integer> pileVal = new Stack<Integer>();
+    public Stack<Object> pileVal = new Stack<Object>();
 
     /**
      * Pile contenant les operateurs (exemple : +, -, etc)
      */
-    public List<Integer> pileOp = new Stack<Integer>();
-
-    /**
-     * Methode ajoute
-     */
-    public void ajoute() {
-    }
-
-    /**
-     * Methode soustraire
-     */
-    public void soustraire() {
-    }
-
-    /**
-     * Methode multiplier
-     */
-    public void multiplier() {
-    }
-
-    /**
-     * Methode diviser
-     */
-    public void diviser() {
-    }
-
-    /**
-     * Methode inferieur
-     */
-    public void inferieur() {
-    }
-
-    /**
-     * Methode superieur
-     */
-    public void superieur() {
-    }
-
-    /**
-     * Methode infouegal
-     */
-    public void infouegal() {
-    }
-
-    /**
-     * Methode supouegal
-     */
-    public void supouegal() {
-    }
-
-    /**
-     * Methode egal
-     */
-    public void egal() {
-    }
-
-    /**
-     * Methode nonegal
-     */
-    public void nonegal() {
-    }
-
-    /**
-     * Methode et
-     */
-    public void et() {
-    }
-
-    /**
-     * Méthode ou
-     */
-    public void ou() {
-    }
-
-    /**
-     * Methode negationEntier
-     */
-    public void negationEntier() {
-    }
-
-    /**
-     * Methode negationBool
-     */
-    public void negationBool() {
-    }
-    
+    public Stack<Integer> pileOp = new Stack<Integer>();    
     
     /**
      * ajouteType : ajoute le type à la pile de type
      */
     public void ajouteType(int typeConstante){
-    	
+    	Yaka.controleT.ajouteType(typeConstante);
     }
     /**
      * ajouteType : ajoute le type de l'ident dont le nom est clef à la pile de type s'il existe
      */
     public void ajouteType(String clef){
+    	if(Yaka.tabIdent.existeIdent(clef)){
+    		Ident id = Yaka.tabIdent.chercheIdent(clef);
+    		Yaka.controleT.ajouteType(id.getType());
+    	}else{
+    		Yaka.erreur.ajouterErreur("Identificateur "+clef+" non défini");
+    	}
     }
     
     /**
      * ajouteOp : ajoute l'operateur à la pile d'op
      */
     public void ajouteOp(int typeOp){
+    	pileOp.push(typeOp);
     }
     
     /**
      * ajouteVal : ajoute l'opérande entier à la pile de val
      */
     public void ajouteVal(int valeur){
+    	pileVal.push(valeur);
     }
     /**
      * ajouteVal : ajoute l'opérande booleen à la pile de val
      */
     public void ajouteVal(boolean valeur){
+    	pileVal.push(valeur);
     }
     
     
