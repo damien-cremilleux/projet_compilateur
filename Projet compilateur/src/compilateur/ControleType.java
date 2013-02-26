8daecfd28c797f57a9fde2dd89182eb7328971e6
Damien CREMILLEUX <damien.cremilleux@insa-rennes.fr>
2013-02-26 09:10:43 +0100

    Définition des constantes de types

--- Projet compilateur/src/compilateur/ControleType.java
+++ Projet compilateur/src/compilateur/ControleType.java
@@ -1,6 +1,6 @@
 /**
  * Project compilateur - 2012/2013
- * date : 19/02/2013
+ * date : 26/02/2013
  * 
  */
 package compilateur;
@@ -8,20 +8,66 @@ package compilateur;
 /**
  * Classe ControleType
  * 
- * @author Samuel COZ - Damien CRÃ‰MILLEUX - Lauriane HOLY - Arnaud TROUCHE
+ * @author Samuel COZ - Damien CREMILLEUX - Lauriane HOLY - Arnaud TROUCHE
  * 
  */
 
-import java.util.List;
 import java.util.Stack;
 
 public class ControleType {
-    public List<String> pileT = new Stack<String> ();
+	/**
+	 * Pile contenant les types (entier, booleen, erreur, etc)
+	 */
+	public static Stack<Integer> pileT = new Stack<Integer>();
 
+	/**
+	 * Ajoute un type sur la pile
+	 * 
+	 * @param type le type à ajouter
+	 */
+	public void ajouteType(int type) {
+		pileT.push(type);
+	}
 
-    public int controlerType() {
-        // TODO Auto-generated return
-        return 0;
-    }
+	/**
+	 * Controle du type pour une opération
+	 * 
+	 * @param op
+	 *            l'opérateur
+	 * 
+	 * 
+	 */
+	public void controlerType(int op) {
+		switch (op) {
+		case Constante.OP_PLUS:
+			if( pileT.pop() == pileT.pop() == Constante.ENTIER
+			int b = pileT.pop()
+			break;
+		case Constante.OP_MOINS:
+			break;
+		case Constante.OP_FOIS:
+			break;
+		case Constante.OP_DIV:
+			break;
+		case Constante.OP_INF:
+			break;
+		case Constante.OP_INFEQ:
+			break;
+		case Constante.OP_SUP:
+			break;
+		case Constante.OP_SUPEQ:
+			break;
+		case Constante.OP_EQ:
+			break;
+		case Constante.OP_DIF:
+			break;
+		case Constante.OP_ET:
+			break;
+		case Constante.OP_OU:
+			break;
+		default:
+			break;
+		}
+	}
 
 }
