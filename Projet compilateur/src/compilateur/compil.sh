626509cc4de12a951266cb0d2eb56b1e07d76699
Damien Crémilleux <damien.cremilleux@insa-rennes.fr>
2013-02-27 19:02:02 +0100

    Suppression du bug relatif à mkdir dans le script

--- Projet compilateur/src/compilateur/compil.sh
+++ Projet compilateur/src/compilateur/compil.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-DEST_DIR="/home/damienc/Desktop/compil/compilateur"
+DEST_DIR="/home/damienc/Desktop/compil/"
+DEST_DIR_COMP="/home/damienc/Desktop/compil/compilateur"
+
 rm -rf $DEST_DIR
 mkdir $DEST_DIR
-cp * $DEST_DIR
-cd $DEST_DIR
+mkdir $DEST_DIR_COMP
+cp * $DEST_DIR_COMP
+cd $DEST_DIR_COMP
 javacc Yaka.jj
 rm TokenMgrError.java ParseException.java Token.java SimpleCharStream.java
 javacc Yaka.jj
+rm Test*
 javac *.java
