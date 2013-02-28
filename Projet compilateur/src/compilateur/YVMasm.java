c9e9a9803c520c6d3d07834e58db06ba77b90733
atrouche <atrouche@PC3-INFO9.educ.insa>
2013-02-28 17:03:46 +0100

    MOIIIII

--- Projet compilateur/src/compilateur/YVMasm.java
+++ Projet compilateur/src/compilateur/YVMasm.java
@@ -30,6 +30,8 @@ public class YVMasm extends YVM {
 	 * Réalise l'entete d'un fichier
 	 */
 	public void entete() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";entete");
 		Ecriture.ecrireStringln(fichierSortie, ".model SMALL");
 		Ecriture.ecrireStringln(fichierSortie, ".586");
 		Ecriture.ecrireStringln(fichierSortie, ".CODE");
@@ -44,11 +46,15 @@ public class YVMasm extends YVM {
 	 *            le nombre de variables
 	 */
 	public void ouvrePrinc(final int nb) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";ouvrePrinc "+nb);
 		Ecriture.ecrireStringln(fichierSortie, "mov bp,sp");
 		Ecriture.ecrireStringln(fichierSortie, "sub sp," + nb);
 	}
 
 	public void iadd() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iadd");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "add ax,bx");
@@ -56,6 +62,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void isub() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";isub");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "sub ax,bx");
@@ -63,6 +71,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void imul() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";imul");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "imul bx");
@@ -70,14 +80,18 @@ public class YVMasm extends YVM {
 	}
 
 	public void idiv() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";idiv");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cwd");
-		Ecriture.ecrireStringln(fichierSortie, "idiv ax,bx");
+		Ecriture.ecrireStringln(fichierSortie, "idiv bx");
 		Ecriture.ecrireStringln(fichierSortie, "push ax");
 	}
 
 	public void inot() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";inot");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "not ax");
 		Ecriture.ecrireStringln(fichierSortie, "push ax");
@@ -85,12 +99,16 @@ public class YVMasm extends YVM {
 	}
 
 	public void ineg() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";ineg");
 		Ecriture.ecrireStringln(fichierSortie, "neg ax");
 		Ecriture.ecrireStringln(fichierSortie, "push ax");
 
 	}
 
 	public void ior() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";ior");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "or ax,bx");
@@ -98,6 +116,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void iand() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iand");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "and ax,bx");
@@ -105,6 +125,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void iinf() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iinf");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cmp ax,bx");
@@ -115,6 +137,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void isup() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";isup");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cmp ax,bx");
@@ -125,6 +149,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void iinfegal() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iinfegal");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cmp ax,bx");
@@ -135,6 +161,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void isupegal() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";isupegal");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cmp ax,bx");
@@ -145,6 +173,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void iegal() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iegal");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cmp ax,bx");
@@ -155,6 +185,8 @@ public class YVMasm extends YVM {
 	}
 
 	public void idiff() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";idiff");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "cmp ax,bx");
@@ -165,21 +197,29 @@ public class YVMasm extends YVM {
 	}
 
 	public void iload(final int offset) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iload");
 		Ecriture.ecrireStringln(fichierSortie, "push word ptr[bp" + offset
 				+ "]");
 	}
 
 	public void istore(final int offset) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";istore");
 		Ecriture.ecrireStringln(fichierSortie, "pop ax");
 		Ecriture.ecrireStringln(fichierSortie, "move word ptr[bp" + offset
 				+ ",ax");
 	}
 
 	public void iconst(final int valeur) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iconst");
 		Ecriture.ecrireStringln(fichierSortie, "push " + valeur);
 	}
 
 	public void ifeq(final String etiquette) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";ifeq");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "cmp 0, bx");
 		Ecriture.ecrireStringln(fichierSortie, "je $+6");
@@ -194,6 +234,8 @@ public class YVMasm extends YVM {
 	 * @param etiquette
 	 */
 	public void iffaux(final String etiquette) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";iffaux");
 		Ecriture.ecrireStringln(fichierSortie, "pop bx");
 		Ecriture.ecrireStringln(fichierSortie, "cmp 0, bx");
 		Ecriture.ecrireStringln(fichierSortie, "jne $+6");
@@ -206,11 +248,15 @@ public class YVMasm extends YVM {
 	 * 
 	 * @param etiquette
 	 */
-	public void gotoYVMasm(final String etiquette) {
+	public void gotoYVM(final String etiquette) {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";gotoYVM");
 		Ecriture.ecrireStringln(fichierSortie, "jump " + etiquette);
 	}
 
 	public void queue() {
+		Ecriture.ecrireStringln(fichierSortie, "");
+		Ecriture.ecrireStringln(fichierSortie, ";queue");
 		Ecriture.ecrireStringln(fichierSortie, "nop");
 		Ecriture.ecrireStringln(fichierSortie, "EXIT CODE");
 		Ecriture.ecrireStringln(fichierSortie, "end debut");
